764298f59daa8c31b78040ddd697ba70bb9f3e2a
Bruce Tran <bruce.tran@gmail.com>
2020-04-20 19:36:52 -0400

    Add project files.

---
+++ ChuckNorrisFacts/ChuckNorrisFacts/FactsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChuckNorrisFacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FactsView : ContentView
+    {
+        public FactsView()
+        {
+            InitializeComponent();
+        }
+
+        private void GetFavoriteClicked(object sender, EventArgs e)
+        {
+        }
+
+        private void GetFactClicked(object sender, EventArgs e)
+        {
+        }
+
+        private void AddFavoriteClicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
