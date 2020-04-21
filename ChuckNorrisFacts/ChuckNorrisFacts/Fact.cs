3ffb98055a5b0288d44ad16fd24886245d21cb89
Bruce Tran <bruce.tran@gmail.com>
2020-04-20 20:14:29 -0400

    Add RestSharp and Newtonsoft.JSON to project. Add logic to Fact methods to call and retrieve from API.

--- ChuckNorrisFacts/ChuckNorrisFacts/Fact.cs
+++ ChuckNorrisFacts/ChuckNorrisFacts/Fact.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ChuckNorrisFacts
+﻿namespace ChuckNorrisFacts
 {
-    class Fact
+    public class Fact
     {
-        public object category { get; set; }
+        public object[] categories { get; set; }
+        public string created_at { get; set; }
         public string icon_url { get; set; }
         public string id { get; set; }
+        public string updated_at { get; set; }
         public string url { get; set; }
         public string value { get; set; }
     }
+
 }
